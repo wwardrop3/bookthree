0eb0ae546f1be7441f3bb18c976aff14977efbb7
Wil Wardrop <wwardrop@gmail.com>
2022-01-12 13:45:11 -0600

    First commit for deconstructing functions.

---
+++ chapter3/scripts/main.js
@@ -0,0 +1,55 @@
+//Steps for grocery shopping
+
+/*
+Individual tasks
+
+1. Determine what meals I want to make this week.
+2. Detail ingredients needed to make the meals.
+3. Take an inventory of items in kitchen.
+4. Make a list of items I need that are not in my kitchen that I need for meals
+5. Grab enough bags to carry the items I plan to purchase
+6. Drive to store.
+7. Shop for groceries
+8. Drive home.
+9. Unbag all groceries.
+10.Put groceries away.
+
+*/
+
+const mealsForWeek = () => {
+    console.log("Determine what meals I want to make this week.")
+}
+
+const ingredientsNeeded = () => {
+    console.log("Detail ingredients needed to make meals")
+
+}
+
+const takeInventory = () => {
+    console.log("Take inventory of food items I already have")
+}
+
+const ingredientsToBuy = () => {
+    console.log("Determine which items I need that I do not already have")
+}
+
+const getBags = () => {
+    console.log("Determine how many bags I need to bring by the amount of items I need to purchase")
+}
+
+const driveToStore = () => {
+    console.log("Drive to store")
+}
+
+const shopForGroceries = () => {
+    console.log("shop for groceries")
+}
+
+const unBagGroceries = () => {
+    console.log("Unbag all groceries")
+}
+
+const putAway = () => {
+    console.log("Put groceries away")
+}
+
